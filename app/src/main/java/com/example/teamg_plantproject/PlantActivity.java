6faf552eea8dd5079150fb0b00cfdda4b242015a
mperlitch <mperlitch55@gmail.com>
2020-03-17 18:00:21 -0400

    Modified Plant activity: enabled camera access and user's picture now gets saved in ImageView. Create two permissions in manifest file

--- app/src/main/java/com/example/teamg_plantproject/PlantActivity.java
+++ app/src/main/java/com/example/teamg_plantproject/PlantActivity.java
@@ -1,14 +1,26 @@
 package com.example.teamg_plantproject;
 
+import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.auth.viewmodel.RequestCodes;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -24,17 +36,23 @@ public class PlantActivity extends AppCompatActivity {
     protected TextView plantName;
     protected TextView plantType;
     protected TextView plantTemp;
+
+    protected ImageView plantPicture;
+    protected Button takePictureButton;
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001 ;
+    Uri image_uri;
+
     protected ProgressBar waterBar;
     protected ProgressBar humidityBar;
     protected ProgressBar sunBar;
     protected int plantID;
-    protected String plantSensorID;
     protected DatabaseHelper db;
     private static final String TAG = "_Plant_Indiv";
     private final int soilMax = 3000;
     private final int solarMax = 2000;
     private FirebaseFirestore fb = FirebaseFirestore.getInstance();
-    private CollectionReference sensorDataRef;
+    private CollectionReference sensorDataRef = fb.collection("sensors/z1QgZ1bVjYnUyrszlU9b/data");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,19 +60,45 @@ public class PlantActivity extends AppCompatActivity {
         setContentView(R.layout.activity_plant);
         setUpUI();
 
+        plantPicture = findViewById(R.id.plant_image_i);
+        takePictureButton = findViewById(R.id.capture_image_btn);
+
+        //button click listener
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //if system is >= Marshmallow, requesrt runtime permission
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                                    PackageManager.PERMISSION_DENIED){
+                        //permission not enabled, request it
+                        String [] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        //show popup to request user permission
+                        requestPermissions(permission, PERMISSION_CODE);
+                    }
+                    else {
+                        //permission is already granted
+                        openCamera();
+                    }
+                }
+                else {
+                    //system < marshmallow
+                    openCamera();
+                }
+            }
+        });
+
         Intent intent = getIntent();
         plantID = intent.getIntExtra("PlantID", 0);
         db = new DatabaseHelper(getApplicationContext());
 
-        plantSensorID = db.getPlant(plantID).getSensorId();
         plantName.setText(db.getPlant(plantID).getPlantName());
         plantType.setText(db.getPlant(plantID).getPlantType());
-        sensorDataRef = fb.collection("sensors/" + plantSensorID + "/data");
-        waterBar.setProgress(25);
-        sunBar.setProgress(50);
-        humidityBar.setProgress(75);
-
-        Log.d(TAG, "onCreate: " + sensorDataRef);
+        waterBar.setProgress(0);
+        humidityBar.setProgress(0);
+        sunBar.setProgress(0);
 
         sensorDataRef.orderBy("createdAt", Query.Direction.DESCENDING).limit(1)
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
@@ -65,6 +109,9 @@ public class PlantActivity extends AppCompatActivity {
                             Log.w(MainActivity.class.getName(), "Listen failed.", e);
                             return;
                         }
+
+                        String sensorDataText = "";
+
                         for (QueryDocumentSnapshot doc : value) {
                             if (doc.get("rawHumidity") != null) {
 
@@ -92,7 +139,47 @@ public class PlantActivity extends AppCompatActivity {
 
     }
 
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
 
+        //Camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //called when image is captured by camera
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            //set the image capture to our ImageView
+            plantPicture.setImageURI(image_uri);
+        }
+    }
+
+    //to handle permissions
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //this method is called when the user presses allow or deny from Permission request popup
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if (grantResults.length > 0 && grantResults[0]
+                        == PackageManager.PERMISSION_GRANTED){
+                    //permission was granted
+                    openCamera();
+                }
+                else {
+                    //permission was denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        }
+    }
     @Override
     public boolean onSupportNavigateUp() {
         Intent intent = new Intent(this, MainActivity.class);
@@ -110,5 +197,7 @@ public class PlantActivity extends AppCompatActivity {
         plantTemp = findViewById(R.id.temerature_i);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+
 
 }
